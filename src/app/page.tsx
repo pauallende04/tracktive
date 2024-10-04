6c3aab7ded5b1bbb075afdbc550a3ba4b0acb514
pauallende <pauallendeherraiz@gmail.com>
2024-10-04 11:33:51 +0200

    lang mod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,24 +22,35 @@ const HomePage: React.FC = () => {
   if (session) {
     return (
       <div>
-        <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
+        <nav className="flex justify-between items-center p-6 bg-gradient-to-r from-emerald-500 via-green-500 to-teal-500 text-white">
           <div className="flex items-center">
             {/* Muestra la imagen de perfil del usuario */}
             <img 
               src={session.user?.image || '/default-profile.png'} 
               alt="User profile" 
-              className="w-10 h-10 rounded-full mr-3  " 
+              className="w-10 h-10 rounded-full mr-3" 
             />
             <span className="mr-2">{session.user?.name}</span>
-            {country && <span >{getFlagEmoji(country)}</span>}
+            {country && <span>{getFlagEmoji(country)}</span>}
           </div>
-          <button onClick={() => signOut()} className="bg-red-500 px-4 py-2 rounded">
-            Sign Out
+    
+          {/* Enlaces a los modos de juego */}
+          <div className="flex-grow text-center">
+          <button className="ml-2 mr-4 text-lg font-medium">Modo Clásico</button>
+            <button className="ml-2 mr-4 text-lg font-medium">Desafío Diario</button>
+            <button className="ml-2 mr-4 text-lg font-medium">Liga Competitiva</button>
+          </div>
+    
+          {/* Botón de cerrar sesión con ícono SVG */}
+          <button onClick={() => signOut()} className="bg-transparent p-2">
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-logout"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M9 12h12l-3 -3" /><path d="M18 15l3 -3" /></svg>
           </button>
         </nav>
         {/* Aquí puedes añadir más contenido para la página principal */}
       </div>
     );
+    
+     
   }
 
   return (
